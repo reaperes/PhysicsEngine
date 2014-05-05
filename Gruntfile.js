27ac5e1b4af3d1ccc7060c595045bc6596d12141
reaperes <reaperes105@gmail.com>
2014-05-05 10:47:20 +0900

    update build files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,30 +2,46 @@ module.exports = function (grunt) {
 
   var srcFiles = [
     'src/npengine/NPEngine.js',
-    'src/npengine/FPS.js',
-    'src/npengine/math/*.js',
     'src/npengine/core/*.js',
-    'src/npengine/database/*.js',
     'src/npengine/display/*.js',
+    'src/npengine/display/npobject/*.js',
+    'src/npengine/display/npobject/base/*.js',
+    'src/npengine/display/npobject/npobject/*.js',
     'src/npengine/display/background/*.js',
-    'src/npengine/display/object/core/*.js',
-    'src/npengine/display/object/*.js',
-    'src/npengine/simulation/*.js',
-    'src/npengine/renderer/*.js'
   ];
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      build: {
+        src: ['bin/npengine.dev.js']
+      }
+    },
 
     concat: {
       dist: {
         src: srcFiles,
         dest: 'bin/npengine.dev.js'
       }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'bin/npengine.js': ['bin/npengine.dev.js']
+        }
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('build', ['concat']);
-}
+  grunt.registerTask(
+      'build',
+      'Compiles all of the assets and copies the files to the build directory.',
+      ['clean', 'concat', 'uglify']
+  );
+};
